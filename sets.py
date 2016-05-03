5708a5c8d149e1270830ee705c864ee88d33f78b
Henry Ingram <hingram@stanford.edu>
2016-05-03 14:57:37 -0700

    Added

---
+++ sets.py
@@ -0,0 +1,17 @@
+class Set: 
+    def __init__(self):
+        pass
+    def contains():
+        pass
+    def add():
+        pass
+    def remove():
+        pass
+    def size():
+        pass
+    def union():
+        pass
+    def intersection():
+        pass
+    def subtract():
+        
